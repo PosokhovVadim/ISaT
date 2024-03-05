4e0c9819e358696b207c265926e78e8c1c8b3e2d
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-03-06 02:04:36 +0400

    implement simple crawler

--- isat/scraper/config/config.py
+++ isat/scraper/config/config.py
@@ -7,6 +7,8 @@ log = logging.getLogger("scraper.log")
 class Config:
     def __init__(self, config_path):
         self.config = self.readConfig(config_path)
+        self.allow_domain = self.config["allow_domain"]
+        self.base_url = self.config["base_url"]
         self.host = self.config["http_server"]["host"]
         self.port = self.config["http_server"]["port"]
 
--- isat/scraper/main.py
+++ isat/scraper/main.py
@@ -1,8 +1,7 @@
-import uvicorn
-import os
 from fastapi import FastAPI
 from isat.pkg.logger.logger import Logger
 from isat.scraper.config.config import Config
+from isat.scraper import crawler
 
 app = FastAPI()
 log = Logger("scraper.log")
@@ -18,13 +17,21 @@ def read_root():
 
 def main():
     log.info("Starting scraper service")
-    cfg = Config(os.getenv("CONFIG_PATH"))
-    log.info(f"Config: {cfg.host}:{cfg.port}")
-    uvicorn.run("isat.scraper.main:app", host=cfg.host, port=cfg.port, reload=True)
+
+    # cfg = Config(os.getenv("CONFIG_PATH"))
+    cfg = Config("isat/scraper/config/config.yaml")
+
+    crw = crawler.Crawler(
+        cfg.base_url, cfg.allow_domain, "isat/scraper/config/urls_data.json"
+    )
+
+    crw.crawl()
+    # log.info(f"Config: {cfg.host}:{cfg.port}")
+    # uvicorn.run("isat.scraper.main:app", host=cfg.host, port=cfg.port, reload=True)
 
 
 # todo:
 # 1) Implement logger - done
 # 2) create cofig  - sooo, why??? - done
-# 3) Implement crowler (async ???)
+# 3) Implement crowler (async ???) - done without async
 # 4) Implement scraper (where contain img, check async impl.) thats all?)
